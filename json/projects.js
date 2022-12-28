9540c36b8fbe53bee336c281d6d5a98d923c0bc1
doublebyte1 <info@doublebyte.net>
2022-12-28 19:12:25 +0000

    - added type: geospatial, ogc-utils

--- json/projects.js
+++ json/projects.js
@@ -1,9 +1,33 @@
 var projectSettings = {
-    "labels": [{
+    "labels": [
+        {
+            "id": "geo",
+            "display_name": "geo",
+            "hex_color": "#bdb32f",
+            "hex_hover_color": "#cfc651",
+            "is_label_font_white": true
+        },        
+        {
+            "id": "ogc-utils",
+            "display_name": "ogc-utils",
+            "hex_color": "#1f97ed",
+            "hex_hover_color": "#74bef2",
+            "is_label_font_white": true
+        }, 
+
+
+        {
             "id": "Valid",
             "display_name": "valid",
             "hex_color": "#178600",
             "hex_hover_color": "#32a852",
+            "is_label_font_white": true
+        },
+        {
+            "id": "Proposal",
+            "display_name": "proposal",
+            "hex_color": "#c20034",
+            "hex_hover_color": "#871e3a",
             "is_label_font_white": true
         },
         {
@@ -26,21 +50,7 @@ var projectSettings = {
             "hex_color": "#beffba",
             "hex_hover_color": "#8bc487",
             "is_label_font_white": false
-        },
-        {
-            "id": "geo",
-            "display_name": "geo",
-            "hex_color": "#bdb32f",
-            "hex_hover_color": "#cfc651",
-            "is_label_font_white": true
-        },        
-        {
-            "id": "ogc-utils",
-            "display_name": "ogc-utils",
-            "hex_color": "#1f97ed",
-            "hex_hover_color": "#74bef2",
-            "is_label_font_white": true
-        },        
+        },       
 
 /*
         {
